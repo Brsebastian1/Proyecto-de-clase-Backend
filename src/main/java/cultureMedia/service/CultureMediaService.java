89ace4963a46fb78ef18597da1e4a00bcaaef777
Brsebastian1 <sebbarrera0@gmail.com>
2024-04-22 08:56:09 -0500

    Implement the logic of business in the service

--- src/main/java/cultureMedia/service/CultureMediaService.java
+++ src/main/java/cultureMedia/service/CultureMediaService.java
@@ -1,12 +1,13 @@
 package cultureMedia.service;
 
+import cultureMedia.exception.CultureMediaException;
 import cultureMedia.model.Video;
 import cultureMedia.model.VideoViews;
 
 import java.util.List;
 
 public interface CultureMediaService {
-    List<Video> findAll();
+    List<Video> findAll() throws CultureMediaException;
     Video add(Video video);
     VideoViews add(VideoViews videoViews);
 }
