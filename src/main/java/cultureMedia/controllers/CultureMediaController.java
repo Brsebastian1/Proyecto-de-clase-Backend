75afeec9a387c27648c25bccea3dc71f44b6cb43
Brsebastian1 <sebbarrera0@gmail.com>
2024-04-29 13:54:43 -0500

    part 10, Code review

--- src/main/java/cultureMedia/controllers/CultureMediaController.java
+++ src/main/java/cultureMedia/controllers/CultureMediaController.java
@@ -1,23 +1,21 @@
 package cultureMedia.controllers;
 
-import cultureMedia.exception.VideoNotFoundException;
+import cultureMedia.exception.CultureMediaException;
 import cultureMedia.model.Video;
-import cultureMedia.service.Impl.CultureMediaServiceImpl;
+import cultureMedia.service.CultureMediaService;
 
 import java.util.List;
 
 public class CultureMediaController {
-    private final CultureMediaServiceImpl cultureMediaService;
+    private final CultureMediaService cultureMediaService;
 
 
-    public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
+    public CultureMediaController(CultureMediaService cultureMediaService) {
         this.cultureMediaService = cultureMediaService;
     }
 
 
-    public List<Video> find_allVideos() throws VideoNotFoundException {
-        List<Video> videos = null;
-        videos = cultureMediaService.findAll();
-        return videos;
+    public List<Video> findAllVideos() throws CultureMediaException {
+        return cultureMediaService.findAll();
     }
 }
